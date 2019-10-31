5e1c33dbc3539cfc9980bd085061d96a0c94bb0c
vplauzon <vplauzon@vplauzon.com>
2019-10-31 13:14:31 -0400

    Function App

--- alert-run-failed/deploy.sh
+++ alert-run-failed/deploy.sh
@@ -7,12 +7,15 @@
 ##
 ##  1- Name of resource group
 ##  2- Storage Account Name
+##  2- Function App Name
 
 rg=$1
 storage=$2
+function=$3
 
 echo "Resource group:  $rg"
 echo "Storage Account Name:  $storage"
+echo "Function App Name:  $function"
 
 echo
 echo "Deploying ARM template"
@@ -20,5 +23,6 @@ echo "Deploying ARM template"
 az group deployment create -n "deploy-$(uuidgen)" -g $rg \
     --template-file deploy.json \
     --parameters \
-    storageAccountName=$storage
+    storageAccountName=$storage \
+    functionAppName=$function
 
